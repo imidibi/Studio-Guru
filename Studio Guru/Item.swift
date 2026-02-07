4243dafc3210311be0d295b3f292d5aa80e7a0cb
Ian Miller <imiller@salesdiver.net>
2026-02-07 12:24:44 -0800

    Initial Commit

---
+++ Studio Guru/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Studio Guru
+//
+//  Created by Ian Miller on 2/7/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
